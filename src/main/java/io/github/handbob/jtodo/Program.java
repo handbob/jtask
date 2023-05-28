95be0492808fd99a78d77b7e137a39adb4c4b976
handbob <handbob@icloud.com>
2023-05-28 23:56:22 +0200

    change project

--- src/main/java/io/github/handbob/jtodo/Program.java
+++ src/main/java/io/github/handbob/jtodo/Program.java
@@ -1,6 +1,7 @@
 package io.github.handbob.jtodo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Program {
