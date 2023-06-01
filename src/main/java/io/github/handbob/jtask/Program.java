f7e3277925344716a432996c58f09e99ebb86e01
handbob <handbob@icloud.com>
2023-06-01 16:42:10 +0200

    change project

--- src/main/java/io/github/handbob/jtask/Program.java
+++ src/main/java/io/github/handbob/jtask/Program.java
@@ -3,48 +3,78 @@ package io.github.handbob.jtask;
 import java.util.ArrayList;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-class TaskList {
-	public int id;
-	public String name;
-	public String description;
-	public boolean done;
+class JTask {
+	private String name;
+	private boolean isDone;
+
+	public JTask(String name, boolean isDone) {
+		this.name = name;
+		this.isDone = isDone;
+	}
+
+	public void setName(String name) {
+		this.name = name;    
+	}
+
+	public void setIsDone(boolean isDone) {
+		this.isDone = isDone;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public boolean getIsDone() {
+		return isDone;
+	}
 }
 
 @RestController
 @SpringBootApplication
 public class Program {
-	ArrayList<TaskList> taskList = new ArrayList<TaskList>();
+	ArrayList<JTask> taskList = new ArrayList<JTask>();
 
+	// Get a list of all tasks
 	@GetMapping("/tasklist")
-	public ArrayList<TaskList> getTaskList() {
+	public ArrayList<JTask> getTaskList() {
+		for (JTask task : taskList) {
+			System.out.println(task.getName() + " " + task.getIsDone());
+		}
 		return taskList;
 	}
 
-	// @GetMapping("/item/{id}")
-	// public void getItemById(int id) {
-	// 	return;
-	// }
-
+	// Add a new task
 	@PostMapping("/task")
-	public void postTask(@RequestBody TaskList task) {
-		task.id = taskList.size();
+	public void postTask(@RequestBody JTask task) {
+		System.out.println(task.getName() + " " + task.getIsDone());
 		taskList.add(task);
 	}
 
-	// @PutMapping("/task")
-	// public void putTask(@RequestBody TodoList task) {
-	// 	todoList.set(task.id, task);
-	// }
+	// Update an existing task
+	@PutMapping("/task")
+	public void putTask(@RequestBody JTask task) {
+		for (int i = 0; i < taskList.size(); i++) {
+			if (taskList.get(i).getName().equals(task.getName())) {
+				taskList.set(i, task);
+			}
+		}
+	}
 
-	@GetMapping("/task")
-	public void deleteTask(@RequestBody TaskList task) {
-		taskList.remove(task.id);
+	// Delete an existing task
+	@DeleteMapping("/task")
+	public void deleteTask(@RequestBody JTask task) {
+		for (int i = 0; i < taskList.size(); i++) {
+			if (taskList.get(i).getName().equals(task.getName())) {
+				taskList.remove(i);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
