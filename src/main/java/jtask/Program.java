70a386795d1bddf5f03c25252f40fb552df685c2
handbob <handbob@icloud.com>
2023-08-19 16:54:47 +0200

    change project

---
+++ src/main/java/jtask/Program.java
@@ -0,0 +1,11 @@
+package jtask;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class Program {
+	public static void main(String[] args) {
+		SpringApplication.run(Program.class, args);
+	}
+}
