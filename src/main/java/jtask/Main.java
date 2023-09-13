89401129058710e5a20c8ff7432a3d7613aac2fd
handbob <handbob@icloud.com>
2023-09-13 08:34:53 +0200

    change project

--- src/main/java/jtask/Main.java
+++ src/main/java/jtask/Main.java
@@ -4,8 +4,8 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 
 @SpringBootApplication
-public class JTask {
+public class Main {
 	public static void main(String[] args) {
-		SpringApplication.run(JTask.class, args);
+		SpringApplication.run(Main.class, args);
 	}
 }
